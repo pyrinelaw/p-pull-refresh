fce2563ff0c28b7b3a6a32d726555fb1ce4829bb
pyrinelaw <pyrine.law@gmail.com>
2015-11-12 11:45:24 +0800

    初始版本

---
+++ README.md
@@ -0,0 +1,81 @@
+# 网页版本下拉刷新
+
+------
+
+手机APP开发中，下拉刷新是一个很常见的功能，但是在网页中，这种模式用的很少。网页下拉刷新，看似简单的功能，但我在网上并没有找到比较好的解决方法。自己开发了一个，期间遇到了各种小坑，浏览器兼容，各种浏览器下拉默认事件，PC端无触摸事件~。记录一下
+
+## 静态样式
+**html代码示例**
+```html
+<div class="container" id="container">
+	<div class="loading-warp">
+	</div>
+</div>
+```
+其中container是触摸容器，loading-warp，是刷新提示容器，容器样式、id可以自定义，此处只是示例而已
+
+**css代码示例**
+```css
+.container{ overflow: hidden; min-height: 100%; }
+.loading-warp{ margin-top: -100px; }
+```
+container高度不能设置为0,以免不能触发触摸事件，overflow属性必须设置为hidden~
+loading-warp的margin-top值必须设置为其自身高度的相反值~
+
+## 引用
+```javascript
+<script type="text/javascript" src="script/lib/jquery-1.11.0.js"></script>
+<script type="text/javascript" src="script/p-pull-refresh.js"></script>
+```
+WAP端引入jquery与下拉插件即可，当然也可以引用zepto
+
+```javascript
+<script type="text/javascript" src="script/lib/jquery-1.11.0.js"></script>
+<script type="text/javascript" src="script/lib/touche.js"></script>
+<script type="text/javascript" src="script/p-pull-refresh.js"></script>
+```
+- PC端需要额外引入touche库文件添加触摸支持 [touche](https://github.com/pyrinelaw/touche);
+- PC端调用需要触摸容器中图片相关元素的draggable为false，否则会触发默认拖动事件，导致下拉刷新失败;
+
+## 调用参数
+```javascript
+{
+     // 触摸容器，默认为body
+    $el: $('body'),	   
+    // 刷新提示容器
+    $loadingEl: null,
+    // 是否自动隐藏
+    autoHide: true, 
+    // 获取下拉刷新发送数据，可以使用静态数据，也可以使用使用function动态传入数据
+    sendData: null,	    
+    // 触发拖动像素距离(触发灵敏度),
+    // 浏览器中下拉默认事件一旦触发后，就不能再通过冒泡阻止此事件。
+    // chrome浏览器中大概是15PX左右的下拉后触发默认刷新，微信中大概是6像素左右。
+    // 如需在微信中使用，建议设置为6像素
+    startPX: 6, 	
+    // 回调函数
+    callbacks: {
+    	pullStart: null,	// 拖动开始
+    	start: null,	// 开始请求数据
+    	success: null,	// 数据请求成功
+    	error: null,	// 数据请求错误
+    	end: null,	// 下拉流程结束
+    }
+}
+```
+## 提供方法
+```javascript
+// 初始化
+reset();
+// 设置销毁状态,statu设置为false后下拉刷新将不再生效，设置为true后将再次生效
+setDestroy(statu);
+```
+
+### 相关资料
+下载地址: [https://github.com/pyrinelaw/p-pull-refresh](https://github.com/pyrinelaw/p-pull-refresh)
+Demo: [http://pyrinelaw.github.io/pRefresh](http://pyrinelaw.github.io/pRefresh)
+Drag介绍文档: [http://www.w3schools.com/tags/ev_ondrag.asp](http://www.w3schools.com/tags/ev_ondrag.asp)
+作者: [Petrus.Law](https://github.com/pyrinelaw)
+
+---
+创建日期: 2015/11/12
---
+++ script/lib/touche.js
@@ -0,0 +1,92 @@
+// Touché: bringing touch events to non-touch browsers https://github.com/davidcalhoun/touche
+(function(){  // shouldn't need to wait DOM readiness (famous last words...)
+  
+  /**
+   * 是否触摸设备
+   */
+  var isTouchDevice = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/);
+  
+  if(isTouchDevice) {
+      // 'looks like touch events are already present, so return early';
+      return;
+  }
+  
+  var isMouseDown = false, // so we don't fire touchmove when the mouse is up
+      originator,
+      fireTouch,
+      mousedown,
+      mousemove,
+      mouseup;
+  
+  fireTouch = function(type, e) {
+    var target,
+        newEvent,
+        touchesObj;
+
+    target = originator || e.target;
+    newEvent = document.createEvent('MouseEvent');  // trying to create an actual TouchEvent will create an error
+    newEvent.initMouseEvent(type, true, true, window, e.detail,
+                            e.screenX, e.screenY, e.clientX, e.clientY,
+                            e.ctrlKey, e.shiftKey, e.altKey, e.metaKey,
+                            e.button, e.relatedTarget);
+    
+    // touches/targetTouches/changedTouches emulation
+    touchesObj = [{
+      // identifier: unique id for the touch event (lazy.. just hooking it into the timestamp)
+      // not using Date.now() just for greater support
+      identifier: (new Date()).getTime(),
+      pageX:      e.pageX,
+      pageY:      e.pageY,
+      clientX:    e.clientX,
+      clientY:    e.clientY,
+      target:     target,
+      screenX:    e.screenX,
+      screenY:    e.screenY
+    }];
+    
+    switch(type) {
+      case 'touchstart':  // e.touches and e.changedTouches and e.targetTouches
+        originator = target;
+        newEvent.touches = newEvent.changedTouches = newEvent.targetTouches = touchesObj;
+      break;
+      
+      case 'touchmove':   // e.touches and e.changedTouches and e.targetTouches
+        newEvent.touches = newEvent.changedTouches = newEvent.targetTouches = touchesObj;
+      break;
+      
+      case 'touchend':    // e.changedTouches only
+        originator = null;
+        newEvent.changedTouches = touchesObj;
+        newEvent.touches = newEvent.targetTouches = [];
+      break;
+      default:
+      break;
+    }
+    
+    // fire off the event!
+    e.target.dispatchEvent(newEvent);
+  }
+  
+  // hook up the mouse->touch mapped listeners
+  mousedown = function(e) {
+    isMouseDown = true;
+    fireTouch('touchstart', e);
+  }
+  mousemove = function(e) {
+    if(!isMouseDown) return;
+    fireTouch('touchmove', e);
+  }
+  mouseup = function(e) {
+    isMouseDown = false;
+    fireTouch('touchend', e);
+  }
+  document.addEventListener('mousedown', mousedown, false);
+  document.addEventListener('mousemove', mousemove, false);
+  document.addEventListener('mouseup', mouseup, false);
+  
+  // old style handlers - only here to get around feature detection (comment if you need to)
+  window.ontouchstart = mousedown;
+  window.ontouchmove  = mousemove;
+  window.ontouchend   = mouseup;
+
+})();
